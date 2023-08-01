6b658c70e66bf26457213c5f521db0329e2b700f
LeeJinIe <jj0745@naver.com>
2023-08-01 17:54:53 +0900

    css 추가 #1

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,8 +1,4 @@
 const imagesLength = 3;
 
 const choseImage = `${Math.floor(Math.random()*imagesLength)}.jpg`;
-
-const bgImage = document.createElement("img");
-bgImage.src = `img/${choseImage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url(img/${choseImage})`;
